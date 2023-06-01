95db9a7e95c98eb379e06b184c727413d00e34bf
hlinhlv2 <levuhoanglinh2k3@gmail.com>
2023-06-01 22:48:20 +0700

    Test app authorization OAUTH2

---
+++ test.js
@@ -0,0 +1,31 @@
+const {google} = require('googleapis');
+
+/**
+ * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
+ * from the client_secret.json file. To get these credentials for your application, visit
+ * https://console.cloud.google.com/apis/credentials.
+ */
+const oauth2Client = new google.auth.OAuth2(
+  "855403999908-ebps3t9l5abdfr75172742tavscd1pgk.apps.googleusercontent.com",
+  "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",
+  "https://google.com"
+);
+
+// Access scopes for read-only Drive activity.
+const scopes = [
+  'https://www.googleapis.com/auth/drive.metadata.readonly'
+];
+
+// Generate a url that asks permissions for the Drive activity scope
+const authorizationUrl = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: 'online',
+  /** Pass in the scopes array defined above.
+    * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+  scope: scopes,
+  // Enable incremental authorization. Recommended as a best practice.
+  include_granted_scopes: true
+});
+
+console.log(oauth2Client);
+console.log(authorizationUrl);
